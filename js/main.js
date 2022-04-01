416677652753b8510ba937b356e2f22d9fa4f0c7
leunets <leunec@mail.ru>
2022-04-01 18:55:50 +0300

    задал имена переменных

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
-const inputCheckbox = document.querySelector ('container-custom-checkbox input'),
+const inputCheckbox = document.querySelector('.container-custom-checkbox input'),
+    ingredients = document.querySelector('.current-pizza-item'),
+    drinks = document.querySelector('.select-drink-item'),
+    totalAmount = document.querySelector('.total-ampunt>.suma'),
+    orderBtn = document.querySelector('.typical-btn'),
+    modalWindow = document.querySelector('.model-window'),
+    submitBtn = document.querySelector('.model-window__submit-btn');
 
